97f2ff5269442b4c7d924e22335f321e0dcaa317
liwenke <949484025@qq.com>
2023-02-20 17:06:29 +0800

    feat: 增加文件读写接口

--- src/main/java/com/hust/tools/Util.java
+++ src/main/java/com/hust/tools/Util.java
@@ -1,10 +1,18 @@
 package com.hust.tools;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileFilter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
+import org.apache.commons.io.filefilter.WildcardFileFilter;
 
 public class Util {
     public static double mean(List<Integer> list) {
@@ -62,6 +70,49 @@ public class Util {
             return lines.toArray(new String[lines.size()]);
         } finally {
             br.close();
+        }
+    }
+
+    public static List<File> listJavaFiles(String dirPath) {
+
+        File topDir = new File(dirPath);
+
+        List<File> directories = new ArrayList<>();
+        directories.add(topDir);
+
+        List<File> textFiles = new ArrayList<>();
+
+        List<String> filterWildcards = new ArrayList<>();
+        filterWildcards.add("*.java");
+
+        FileFilter typeFilter = new WildcardFileFilter(filterWildcards);
+
+        while (directories.isEmpty() == false) {
+            List<File> subDirectories = new ArrayList<File>();
+            for (File f : directories) {
+                subDirectories.addAll(Arrays.asList(f.listFiles((FileFilter) DirectoryFileFilter.INSTANCE)));
+                textFiles.addAll(Arrays.asList(f.listFiles(typeFilter)));
+            }
+            directories.clear();
+            directories.addAll(subDirectories);
+        }
+        Collections.sort(textFiles);
+        return textFiles;
+    }
+
+    public static void writeFile(String allLines, String fileName, boolean append) {
+        File aFile = new File(fileName);
+        FileWriter aFileWriter;
+        try {
+            if (aFile.exists() == false)
+                aFile.createNewFile();
+            aFileWriter = new FileWriter(aFile, append);
+            {
+                aFileWriter.write(allLines);
+            }
+            aFileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
