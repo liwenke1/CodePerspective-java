401072e0728bf5bdc870087cf6a3f474f835471b
liwenke <949484025@qq.com>
2023-02-21 15:45:28 +0800

    fix: 修改结果输出

--- src/main/java/com/hust/output/DictResult.java
+++ src/main/java/com/hust/output/DictResult.java
@@ -3,13 +3,19 @@ package com.hust.output;
 import java.util.Map;
 
 public class DictResult implements Result {
-    public Map<String, Double> value;
+    private Map<String, Double> value;
 
     public DictResult(Map<String, Double> value) {
         this.value = value;
     }
 
-    public double getResult() {
+    @Override
+    public double getScalarResult() {
         return 0;
+    }
+
+    @Override
+    public Map<String, Double> getDictResult() {
+        return value;
     }
 }
--- src/main/java/com/hust/output/ScalarResult.java
+++ src/main/java/com/hust/output/ScalarResult.java
@@ -1,13 +1,23 @@
 package com.hust.output;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ScalarResult implements Result {
-    public double value = 0;
+    private double value = 0;
 
     public ScalarResult(double value) {
         this.value = value;
     }
 
-    public double getResult() {
+    @Override
+    public double getScalarResult() {
         return value;
+    }
+
+    @Override
+    public Map<String, Double> getDictResult() {
+        Map<String, Double> rtn = new HashMap<>();
+        return rtn;
     }
 }
